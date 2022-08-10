856b1838d61bc3dda914d31c267cda8e02da4126
Doris Mosquera Lozano <doris.mosquera@sofka.local>
2022-08-10 09:42:47 -0500

    Backend básico listo

---
+++ models/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+
+import {router} from "../models/routes/usuarios.js";
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath = '/api/usuarios';
+
+    //Middlewares
+    this.middlewares();
+
+    //Rutas de mi aplicación
+    this.routes();
+  }
+
+  middlewares() {
+
+    //CORS
+    this.app.use(cors());
+
+    //Lectura y parseo del body
+    this.app.use(express.json());
+
+    //Directorio Público
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(this.usuariosPath, router );
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en el puerto", this.port);
+    });
+  }
+}
+
+export default Server;
